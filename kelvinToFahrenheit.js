cce960b5cbc8c10d123dc16975a48ef75e1a58ee
Henry Do <36933624+henrydo3000@users.noreply.github.com>
2019-06-23 22:48:27 -0700

    JavaScript code to convert Kelvin to Fahrenheit.

---
+++ kelvinToFahrenheit.js
@@ -0,0 +1,5 @@
+const kelvin = 0; //declare variable kelvin and assigns it value 293
+let celsius = kelvin - 273; // converts kelvin to celsius
+let fahrenheit = celsius * (9/5) + 32; //converts celsius to fahrenheit 
+fahrenheit = Math.floor(fahrenheit); //rounds down decimal values of fahrenheit   to integerand assigns it value to fahrenheit.
+console.log(`the temperature is ${fahrenheit} degrees Fahrenheit`); // logs the value of fahrenheit onto the console.
